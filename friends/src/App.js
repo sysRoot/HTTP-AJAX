b52ff079cff1a60ac8a8befa09d8875ed447dd85
Jason Cook <the.root.user@gmail.com>
2019-04-18 22:39:10 -0400

    Stumped, but still trying

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,64 +2,69 @@ import React, { Component } from "react";
 
 import "./App.css";
 import Friends from "./components/Friends";
-import axios from "axios";
+// import axios from "axios";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      friends: [],
-      name: ``,
-      age: ``,
-      email: ``
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     friends: [],
+  //     name: ``,
+  //     age: 0,
+  //     email: ``,
+  //     editing: false
+  //   };
+  // }
 
-  componentDidMount() {
-    axios
-      .get(`http://localhost:5000/friends`)
-      .then(res => {
-        console.log(res.data, `RESPONSE`);
-        this.setState({ friends: res.data });
-      })
-      .catch(err => console.log(err));
-  }
+  // componentDidMount() {
+  //   axios
+  //     .get(`http://localhost:5000/friends`)
+  //     .then(res => {
+  //       console.log(res.data, `RESPONSE`);
+  //       this.setState({ friends: res.data });
+  //     })
+  //     .catch(err => console.log(err));
+  // }
 
-  addFriendHandler = (event) => {
-    event.preventDefault();
-    axios
-      .post("http://localhost:5000/friends", {
-        name: this.state.name,
-        age: this.state.age,
-        email: this.state.email
-      })
-      .then(res =>
-        this.setState(prevState => {
-          return {
-            friends: res.data
-          };
-        })
-      )
-      .catch(err => console.log(err, `add friend hiccup`));
-        //Blank state for next input
-      this.setState({ name: ``, age: ``, email: `` });
+  // addFriendHandler = event => {
+  //   event.preventDefault();
+  //   axios
+  //     .post("http://localhost:5000/friends", {
+  //       name: this.state.name,
+  //       age: parseInt(this.state.age),
+  //       email: this.state.email
+  //     })
+  //     .then(res =>
+  //       this.setState(prevState => {
+  //         return {
+  //           friends: res.data
+  //         };
+  //       })
+  //     )
+  //     .catch(err => console.log(err, `add friend hiccup`));
+  //   //Blank state for next input
+  //   this.setState({ name: ``, age: 0, email: `` });
+  // };
 
-  }
-  changeHandler = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  }
+  // changeHandler = event => {
+  //   this.setState({
+  //     [event.target.name]: event.target.value
+  //   });
+  // };
+
+
   render() {
     return (
       <div className="App">
         <Friends
-          addFriend={this.addFriendHandler}
-          friends={this.state.friends}
-          changer={this.changeHandler}
-          age={this.state.age}
-          name={this.state.name}
-          email={this.state.email}
+          // addFriend={this.addFriendHandler}
+          // friends={this.state.friends}
+          // changer={this.changeHandler}
+          // age={this.state.age}
+          // name={this.state.name}
+          // email={this.state.email}
+          // editing={this.state.editing}
+          // edit={this.editHandler}
         />
       </div>
     );
--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,44 +1,142 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-const Friends = props => {
-  return (
-    <>
-      <h2>Friends List</h2>
-      <form action="submit" onSubmit={props.addFriend}>
-        <input
-          type="text"
-          name="name"
-          onChange={props.changer}
-          value={props.name}
-          placeholder="Name"
-        />
-        <input
-          type="number"
-          name="age"
-          onChange={props.changer}
-          value={props.age}
-          placeholder="Age"
-        />
-        <input
-          type="email"
-          name="email"
-          onChange={props.changer}
-          value={props.email}
-          placeholder="E-Mail"
-        />
-        <button type="submit">Add Friend</button>
-      </form>
-      {props.friends.map(cur => {
-        return (
-          <div key={cur.id}>
-            <p>{cur.name}</p>
-            <p>{cur.age}</p>
-            <p>{cur.email}</p>
-          </div>
-        );
-      })}
-    </>
-  );
-};
+class Friends extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: [],
+      name: ``,
+      age: null,
+      email: ``,
+
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/friends`)
+      .then(res => {
+        console.log(res.data, `RESPONSE`);
+        this.setState({ friends: res.data });
+      })
+      .catch(err => console.log(err));
+  }
+
+  addFriendHandler = event => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:5000/friends", {
+        name: this.state.name,
+        age: parseInt(this.state.age),
+        email: this.state.email,
+      })
+      .then(res =>
+        this.setState(prevState => {
+          return {
+            friends: res.data
+          };
+        })
+      )
+      .catch(err => console.log(err, `add friend hiccup`));
+    //Blank state for next input
+    this.setState({ name: ``, age: null, email: `` });
+  };
+
+  updateFriendHandler = event => {
+    axios
+      .put(`http://localhost:5000/friends/${this.state.id}`, {
+        name: this.state.name,
+        age: parseInt(this.state.age),
+        email: this.state.email
+      })
+      .then(res => {
+        this.setState({ friends: res.data });
+      })
+      .catch(err => console.log(err));
+    this.setState({ name: ``, age: null, email: `` });
+  };
+
+  changeHandler = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  editHandler = (event) => {
+
+  };
+
+  render() {
+    const view = {};
+    const edit = {};
+    
+    return (
+      <>
+        <h2>Friends List</h2>
+        <form action="submit" onSubmit={this.addFriendHandler}>
+          <input
+            type="text"
+            name="name"
+            onChange={this.changeHandler}
+            value={this.props.name}
+            placeholder="Name"
+          />
+          <input
+            type="number"
+            name="age"
+            onChange={this.changeHandler}
+            value={this.props.age}
+            placeholder="Age"
+          />
+          <input
+            type="email"
+            name="email"
+            onChange={this.changeHandler}
+            value={this.props.email}
+            placeholder="E-Mail"
+          />
+          <button type="submit">Add Friend</button>
+        </form>
+        {this.state.friends.map(cur => {
+          return (
+            <>
+              <div
+                key={cur.id}
+                onDoubleClick={this.editHandler}
+                style={view}
+              >
+                <p>{cur.name}</p>
+                <p>{cur.age}</p>
+                <p>{cur.email}</p>
+              </div>
+              <form action="submit" style={edit} onDoubleClick={this.editHandler}>
+                <input
+                  type="text"
+                  onChange={this.changeHandler}
+                  placeholder={cur.name}
+                  value={this.props.name}
+                />
+                <input
+                  type="age"
+                  onChange={this.changeHandler}
+                  placeholder={cur.age}
+                  value={this.props.age}
+                />
+                <input
+                  onDoubleClick={this.editHandler}
+                  type="email"
+                  onChange={this.changeHandler}
+                  placeholder={cur.email}
+                  value={this.props.email}
+                />
+              </form>
+            </>
+          );
+        })}
+      </>
+    );
+  }
+}
 
 export default Friends;
