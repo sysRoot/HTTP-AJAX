f4c0a2457add67ad151a9c2c54f78a42352c5e59
Jason Cook <the.root.user@gmail.com>
2019-06-11 23:21:55 -0400

    Commit axios homework

---
+++ friends/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./App.css";
+import Friends from "./components/Friends";
+// import axios from "axios";
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Friends />
+      </div>
+    );
+  }
+}
+
+export default App;
+
